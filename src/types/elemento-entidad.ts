cd0e30c205b242df0ce41352421a85e48ff823a9
RogerJackD <rogerzapata114@gmail.com>
2026-01-14 23:11:22 -0500

    feat(types): add entidad and elemento entity interfaces

---
+++ src/types/elemento-entidad.ts
@@ -0,0 +1,75 @@
+export interface EntidadSistema {
+  idEntidadSistema: number;
+  nombreEntidadSistema: string;
+  indicadorEstado: string;
+  idModuloSistema: number;
+  usuarioRegistro: string;
+  fechaRegistro: string;
+  usuarioModificacion: string | null;
+  fechaModificacion: string | null;
+  ultimaFechaDescarga: string | null;
+  ultimaFechaSubida: string | null;
+  numeroRegistrosPendientesIteracion: number;
+  numeroRegistrosPendientesSincronizacion: number;
+  modoSincronizacion: string;
+  numeroFilasPorPagina: number;
+  estadoImportacion: string;
+  nombrePlantillaImportacion: string;
+}
+
+export interface CreateElementoEntidadDto {
+  idEntidad: string;
+  nombreElemento: string;
+  visible: string;
+  editable: string;
+  indicadorEstado: string;
+  usuarioRegistro: string;
+}
+
+export interface ElementoEntidad {
+  id: number;
+  idEntidad: number;
+  nombreElemento: string;
+  visible: string;
+  editable: string;
+  indicadorEstado: string;
+  usuarioRegistro: string;
+  fechaRegistro: string;
+  usuarioModificacion: string | null;
+  fechaModificacion: string | null;
+  entidad: EntidadSistema;
+}
+
+export interface CreateEntidadSistemaDto {
+  nombreEntidadSistema: string;
+  indicadorEstado?: 'A' | 'E';
+  idModuloSistema: number;
+  usuarioRegistro: string;
+  numeroRegistrosPendientesIteracion?: number;
+  numeroRegistrosPendientesSincronizacion?: number;
+  modoSincronizacion?: '0' | '1' | '2' | '3';
+  numeroFilasPorPagina?: number;
+  estadoImportacion?: string;
+  nombrePlantillaImportacion?: string;
+}
+
+
+export interface UpdateEntidadSistemaDto {
+  nombreEntidadSistema?: string;
+  indicadorEstado?: 'A' | 'E';
+  idModuloSistema?: number;
+  numeroRegistrosPendientesIteracion?: number;
+  numeroRegistrosPendientesSincronizacion?: number;
+  modoSincronizacion?: '0' | '1' | '2' | '3';
+  numeroFilasPorPagina?: number;
+  estadoImportacion?: string;
+  nombrePlantillaImportacion?: string;
+  usuarioModificacion: string;
+}
+
+export interface UpdateElementoEntidadDto {
+  nombreElemento?: string;
+  visible?: string;
+  editable?: string;
+  usuarioModificacion: string;
+}
